9250a7aa491a517c1fa1e1083cf718214a4c38e4
huber <huber@hubers-MacBook-Pro-2.local>
2015-10-07 00:09:02 -0300

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# starter-material-and-form
---
+++ form_ajax/libform/config_landing.php
@@ -0,0 +1,8 @@
+<?PHP
+// Parametros definidos por cada cliente:
+
+$email_saliente = "hubermann@gmail.com"; // Email que envía los contactos recibidos en el formulario.
+$email_recibe = "hubermann@gmail.com"; // Email que recibe los contactos recibidos.
+$asunto_email = "MSG desde web"; //Asunto del email recibido.
+
+?>
